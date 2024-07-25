80fc3bcd0b5cda8e6ba6c354a8c85f10e7278a6e
Will Dunlap <wdunlap@phdata.io>
2024-07-25 21:56:15 +0000

    add a note

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,6 +1,6 @@
 
 /*
-    Welcome to your first dbt model!
+    Welcome to your first dbt model! a note
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
